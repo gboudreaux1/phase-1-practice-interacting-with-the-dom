041c3833f07c6c23a7770f4733cd44f23244a61c
xauvk <xauvkubalexanderyang@live.com>
2022-06-30 11:03:13 -0400

    added formSubmit, minusElement, plusElement, and counterElement

--- js/challenge.js
+++ js/challenge.js
@@ -1,7 +1,27 @@
-function minusButton() {
-const newNumber = document.getElementById('counter')
-newNumber.textContent += 1
+// function minusButton() {
+//     const newNumber = document.getElementById('counter')
+//     newNumber.textContent += 1   
+// }
 
 
-    
+
+
+const formElement = document.querySelector('form')
+
+function formSubmit(e) {
+    e.preventDefault()
+    const userText = e.target.comma.value;
+    if (userText.length > 0) {
+        const commentContainer = document.getElementById('list')
+        const newElement = document.createElement('li')
+        newElement.textContent = userText
+        commentContainer.append(newElement)
+        e.target.reset()
+    }
 }
+
+formElement.addEventListener('submit', formSubmit)
+
+const minusElement = document.getElementById('minus')
+const plusElement = document.getElementById('plus')
+const counterElement = document.getElementById('counter')
